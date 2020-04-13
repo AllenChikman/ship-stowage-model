a7adad0eb21c51d4a3c65c0703d2d19769e201ec
or morshtein <ormorshtein@mail.tau.ac.il>
2020-04-13 17:52:46 +0300

    Refactoring

--- src/StowageAlgorithm.cpp
+++ src/StowageAlgorithm.cpp
@@ -8,26 +8,21 @@
 #include "Ship.h"
 #include "WeightBalanceCalculator.h"
 
-bool isShipFull(ShipPlan *shipPlan)
-{
+bool isShipFull(ShipPlan *shipPlan) {
     bool shipVacant = false;
     unsigned length = shipPlan->getLength();
     unsigned width = shipPlan->getWidth();
     unsigned maxHeight = shipPlan->getHeight();
 
-    for(unsigned x = 0; x<length; x++)
-    {
-        for(unsigned y = 0; x<width; y++)
-        {
-            if(shipPlan->getFreeCells()[x][y] < maxHeight)
-            {
+    for (unsigned x = 0; x < length; x++) {
+        for (unsigned y = 0; y < width; y++) {
+            if (shipPlan->getFreeCells()[x][y] < maxHeight) {
                 shipVacant = true;
                 break;
             }
 
         }
-        if(shipVacant)
-        {
+        if (shipVacant) {
             break;
         }
     }
@@ -35,13 +30,10 @@ bool isShipFull(ShipPlan *shipPlan)
 
 }
 
-bool containerHasIllegalDestPort(std::vector<Port> shipRoute, Container const &container)
-{
+bool containerHasIllegalDestPort(std::vector<Port> shipRoute, Container const &container) {
     bool legalDestPort = false;
-    for(auto &port : shipRoute)
-    {
-        if(container.getDestinationPort().toStr() == port.getPortID().toStr())
-        {
+    for (auto &port : shipRoute) {
+        if (container.getDestinationPort().toStr() == port.getPortID().toStr()) {
             legalDestPort = true;
             break;
         }
@@ -49,11 +41,11 @@ bool containerHasIllegalDestPort(std::vector<Port> shipRoute, Container const &c
     return !legalDestPort;
 }
 
-bool isBalanced()
-{
+bool isBalanced() {
 //#TODO: implement
     return false;
 }
+
 void organizeCargoDataToList(std::vector<CargoData> &cargoData, const std::string &inputPath) {
     std::vector<std::vector<std::string>> vecLines;
     readToVecLine(inputPath, vecLines);
@@ -63,7 +55,8 @@ void organizeCargoDataToList(std::vector<CargoData> &cargoData, const std::strin
 
 }
 
-void dumpInstruction(std::ofstream &instructions, char op, Container const &container, unsigned x, unsigned y, unsigned z) {
+void
+dumpInstruction(std::ofstream &instructions, char op, Container const &container, unsigned x, unsigned y, unsigned z) {
     instructions << "<" << op << ">" << ",";
     instructions << "<" << container.getID() << ">" << ",";
     instructions << "<" << z << ">" << ",";
@@ -72,9 +65,9 @@ void dumpInstruction(std::ofstream &instructions, char op, Container const &cont
 }
 
 void updateShipPlan(const Container &container, std::ofstream &outputFile, ShipPlan *shipPlan,
-                    Operation op, unsigned x, unsigned y, unsigned z)  {
+                    Operation op, unsigned x, unsigned y, unsigned z) {
 
-    unsigned  newFreeCell;
+    unsigned newFreeCell;
     auto &containerMat = shipPlan->getCargo();
     bool freeCellFound = false;
 
@@ -88,12 +81,9 @@ void updateShipPlan(const Container &container, std::ofstream &outputFile, ShipP
             break;
         case Operation::L:
             // choosing a free cell in a naive way
-            for(int i=0;i<shipPlan->getLength();i++)
-            {
-                for(int j=0; j<shipPlan->getWidth();j++)
-                {
-                    if(shipPlan->getFreeCells()[i][j]<shipPlan->getHeight())
-                    {
+            for (int i = 0; i < shipPlan->getLength(); i++) {
+                for (int j = 0; j < shipPlan->getWidth(); j++) {
+                    if (shipPlan->getFreeCells()[i][j] < shipPlan->getHeight()) {
                         x = i;
                         y = j;
                         z = shipPlan->getFreeCells()[i][j];
@@ -103,8 +93,7 @@ void updateShipPlan(const Container &container, std::ofstream &outputFile, ShipP
                         break;
                     }
                 }
-                if(freeCellFound)
-                {
+                if (freeCellFound) {
                     break;
                 }
             }
@@ -128,7 +117,6 @@ void getInstructionsForCargo(const std::string &inputPath, const std::string &ou
     const auto startingHeightMat = shipPlan->getStartingHeight();
     auto cargoMat = shipPlan->getCargo();
 
-    bool unloadContainersToMove = false;
     const unsigned length = shipPlan->getLength();
     const unsigned width = shipPlan->getWidth();
     const unsigned height = shipPlan->getHeight();
@@ -137,28 +125,37 @@ void getInstructionsForCargo(const std::string &inputPath, const std::string &ou
 
     for (unsigned x = 0; x < length; x++) {
         for (unsigned y = 0; y < width; y++) {
-            for (unsigned z = startingHeightMat[x][y]; z < height; z++) {
-                if (seaPortCodeStr !=    cargoMat[x][y][z].getDestinationPort().toStr()) {
-                    if(unloadContainersToMove && cargoMat[x][y][z].getDestinationPort().toStr() != "") {
-                        updateShipPlan(cargoMat[x][y][z], outputFile, shipPlan, Operation::U, x, y, z);
-                        containersToLoad.insert(containersToLoad.begin(), cargoMat[x][y][z]);
-                    }
-                    continue;
+            unsigned z = startingHeightMat[x][y];
+            unsigned startingIdx = height;
+
+            // 1st step: finding a container to load to current port
+            while (z < height && !cargoMat[x][y][z].getDestinationPort().toStr().empty()) //#TODO
+            {
+                if (seaPortCodeStr == cargoMat[x][y][z].getDestinationPort().toStr()) {
+                    startingIdx = z;
                 }
-                unloadContainersToMove = true;
-                updateShipPlan(cargoMat[x][y][z], outputFile, shipPlan, Operation::U, x, y, z);
+                z++;
             }
-            unloadContainersToMove = false;
+            z = startingIdx;
+
+            // 2nd step: unloading all containers above and marking the ones to be reloaded
+            while (z < height && !cargoMat[x][y][z].getDestinationPort().toStr().empty()) {
+                auto curContainer = cargoMat[x][y][z];
+                if (seaPortCodeStr != curContainer.getDestinationPort().toStr()) {
+                    containersToLoad.push_back(cargoMat[x][y][z]);
+                }
+                updateShipPlan(cargoMat[x][y][z], outputFile, shipPlan, Operation::U, x, y, z);
+                z++;
+            }
         }
     }
     for (const auto &newCargo : cargoDataVec) {
         Container newContainer(newCargo.weight, newCargo.destPort, newCargo.id);
         containersToLoad.push_back(newContainer);
     }
-    for (const auto &c : containersToLoad) {
-        updateShipPlan(c, outputFile, shipPlan, Operation::L);
+    for (const auto &curContainerToLoad : containersToLoad) {
+        updateShipPlan(curContainerToLoad, outputFile, shipPlan, Operation::L);
     }
-
 
 
 }
