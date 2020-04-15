12434b1718223300e2ec01ab77f4b4eb83bbb507
Allen <allenchikman@gmail.com>
2020-04-15 20:30:12 +0300

    giving API for last port visit

--- src/StowageAlgorithm.h
+++ src/StowageAlgorithm.h
@@ -11,8 +11,10 @@ class Algorithm
 {
 
 };
+
 bool getInstructionsForCargo(const std::string &inputPath, const std::string &outputPath, ShipPlan *shipPlan,
-                             const SeaPortCode &curSeaPortCode, const std::vector<SeaPortCode> &shipRoute);
+                             const SeaPortCode &curSeaPortCode, const std::vector<SeaPortCode> &shipRoute,
+                             bool isLastPortVisit);
 
 
 #endif //SHIP_STOWAGE_MODEL_STOWAGE_ALGORITHM_H
