610ca7844712b817a6d323f87214590ebd545056
or morshtein <ormorshtein@mail.tau.ac.il>
2020-05-16 17:52:00 +0300

    Adding computation of containers id

--- src/Container.cpp
+++ src/Container.cpp
@@ -1,6 +1,9 @@
 #include "Container.h"
 #include <algorithm>
 #include <regex>
+#include <string>
+#include <map>
+#include <math.h>
 
 
 SeaPortCode::SeaPortCode(std::string str)
@@ -16,6 +19,20 @@ bool SeaPortCode::isValidCode(const std::string &portSymbol)
     if (portSymbol.size() != 5) { return false; }
     return std::regex_match(portSymbol, std::regex("^[A-Za-z]+$"));
 }
+bool checkDigitMatch(const std::string &id, const std::string &checkDig)
+{
+    std::map<char, int> letterMap = {{'A',10}, {'B',12}, {'C',13}, {'D',14}, {'E',15}, {'F',16}, {'G',17}, {'H',18}, {'I',19}, {'J',20}, {'K',21}, {'L',23}, {'M',24}, {'N',25}, {'O',26}, {'P',27}, {'Q',28}, {'R',29}, {'S',30}, {'T',31}, {'U',32}, {'V',34}, {'W',35}, {'X',36}, {'Y',37}, {'Z',38}, {'0',0},{'1',1},{'2',2},{'3',3},{'4',4},{'5',5},{'6',6},{'7',7},{'8',8},{'9',9}};
+    double sum = 0.0, tmp = 0.0;
+    int n = (int)id.length() - 1;
+    int u = 0;
+    for (int i = 0; i < n; i++)
+    {
+        u = letterMap[id[i]] * (int)pow(2,i);
+        sum += u;
+    }
+    tmp = floor(sum/11) * 11;
+    return (sum-tmp) == letterMap[checkDig];
+}
 bool Container::isValidID() const
 {
 
@@ -28,23 +45,8 @@ bool Container::isValidID() const
     bool sameSec1 = std::regex_match(ownerCode, std::regex("^[A-Z]+$"));
     bool sameSec2 = std::regex_match(categoryId, std::regex("^[U,Z,J]+$"));
     bool sameSec3 = std::regex_match(serialNum, std::regex("^[0-9]+$"));
-    bool sameSec4 = std::regex_match(checkDig, std::regex("^[0-9]+$"));
+    bool sameSec4 = checkDigitMatch(id, checkDig);
     return sameSec1 && sameSec2 && sameSec3 && sameSec4;
 }
-//bool Container::isValidID(const string& id)  {
-//    std::map<char, int> mymap = {{'A',10}, {'B',12}, {'C',13}, {'D',14}, {'E',15}, {'F',16}, {'G',17}, {'H',18}, {'I',19}, {'J',20}, {'K',21}, {'L',23}, {'M',24}, {'N',25}, {'O',26}, {'P',27}, {'Q',28}, {'R',29}, {'S',30}, {'T',31}, {'U',32}, {'V',34}, {'W',35}, {'X',36}, {'Y',37}, {'Z',38}, {'0',0},{'1',1},{'2',2},{'3',3},{'4',4},{'5',5},{'6',6},{'7',7},{'8',8},{'9',9}};
-//    std::regex reg = std::basic_regex("[A-Z]{3}[UZJ][0-9]{6}[0-9]");
-//    std::cout << std::regex_match(id, reg) << std::endl;
-//    double sum = 0;
-//    int n = (int)id.length() - 1;
-//    double tmp = 0.0;
-//    for (int i = 0; i < n; i++)
-//    {
-//        int u = mymap[id[i]] * pow(2,i);
-//        sum += u;
-//    }
-//    tmp = sum/11;
-//    tmp = floor(tmp);
-//    tmp = tmp * 11;
-//    std::cout << ((sum-tmp) == mymap[(id[n])]) << std::endl;
-//}
+
+
