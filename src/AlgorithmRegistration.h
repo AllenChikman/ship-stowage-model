d67055cf3a751604d32b13108b0a02da4bcb9b9b
or morshtein <ormorshtein@mail.tau.ac.il>
2020-05-19 20:28:24 +0300

    trivial

--- src/AlgorithmRegistration.h
+++ src/AlgorithmRegistration.h
@@ -6,12 +6,13 @@
 #include <memory>
 #include "AbstractAlgorithm.h"
 
+#define REGISTER_ALGORITHM(class_name) \
+AlgorithmRegistration register_me_##class_name \
+    ([]{return std::make_unique<class_name>();} );
+
 class AlgorithmRegistration
 {
 public:
     AlgorithmRegistration(std::function<std::unique_ptr<AbstractAlgorithm>()>);
 };
 
-#define REGISTER_ALGORITHM(class_name) \
-AlgorithmRegistration register_me_##class_name \
-    ([]{return std::make_unique<class_name>();} );
